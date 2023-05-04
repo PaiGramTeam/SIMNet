1e9d24238dcbc03ebf023ee6e0268a0ebea5c855
洛水居室 <luoshuijs@outlook.com>
2023-05-04 23:13:53 +0800

    :bug: Fix Bug and Update Document

--- simnet/client/account/auth.py
+++ simnet/client/account/auth.py
@@ -24,9 +24,14 @@ class AuthClient(BaseClient):
 
         Returns:
             bool: `True` if the super ticket successfully retrieved, otherwise `False`.
+
+        Raises:
+            ValueError: If `login_ticket` is not found in the cookies.
         """
         url = AUTH_URL.get_url(Region.CHINESE).join("getMultiTokenByLoginTicket")
         login_ticket = self.cookies.get("login_ticket")
+        if login_ticket is None:
+            raise ValueError("login_ticket not found in cookies.")
         params = {
             "login_ticket": login_ticket,
             "uid": self.account_id,
@@ -53,10 +58,16 @@ class AuthClient(BaseClient):
 
         Returns:
             bool: `True` if the cookie token was successfully retrieved, otherwise `False`.
+
+        Raises:
+            ValueError: If `stoken` is not found in the cookies.
         """
+        stoken = self.cookies.get("stoken")
+        if stoken is None:
+            raise ValueError("stoken not found in cookies.")
         url = GET_COOKIES_TOKEN_BY_STOKEN_URL.get_url(Region.CHINESE)
         params = {
-            "stoken": self.cookies.stoken,
+            "stoken": stoken,
             "uid": self.account_id,
         }
         data = await self.request_lab(url, params=params)
@@ -73,10 +84,16 @@ class AuthClient(BaseClient):
 
         Returns:
             bool: `True` if the login token was successfully retrieved, otherwise `False`.
+
+        Raises:
+            ValueError: If `stoken` is not found in the cookies.
         """
+        stoken = self.cookies.get("stoken")
+        if stoken is None:
+            raise ValueError("stoken not found in cookies.")
         url = GET_LTOKEN_BY_STOKEN_URL.get_url(Region.CHINESE)
         params = {
-            "stoken": self.cookies.stoken,
+            "stoken": stoken,
             "uid": self.account_id,
         }
         data = await self.request_lab(url, params=params)
@@ -87,9 +104,7 @@ class AuthClient(BaseClient):
             return True
         return False
 
-    async def get_authkey_by_stoken(
-        self, game_biz: str, region: str, auth_appid: str
-    ) -> Optional[str]:
+    async def get_authkey_by_stoken(self, game_biz: str, region: str, auth_appid: str) -> Optional[str]:
         """
         Get the auth key (`authkey`) for a game and region using a super ticket (`stoken`).
 
@@ -101,7 +116,13 @@ class AuthClient(BaseClient):
 
         Returns:
             str or None: The authentication key, or None if not found.
+
+        Raises:
+            ValueError: If `stoken` is not found in the cookies.
         """
+        stoken = self.cookies.get("stoken")
+        if stoken is None:
+            raise ValueError("stoken not found in cookies.")
         url = AUTH_KEY_URL.get_url(self.region)
         json = {
             "auth_appid": auth_appid,
@@ -112,17 +133,21 @@ class AuthClient(BaseClient):
         data = await self.request_lab(url, data=json)
         return data.get("authkey")
 
-    async def get_hk4e_token_by_cookie_token(
-        self, game_biz: str, region: str
-    ) -> NoReturn:
+    async def get_hk4e_token_by_cookie_token(self, game_biz: str, region: str) -> NoReturn:
         """
-        Get HK4E token (`stoken`) using cookie token (`cookie_token`).
+        Get HK4E token (`hk4e_token`) using cookie token (`cookie_token`).
         The resulting HK4E token will be automatically saved in self.cookies.
 
         Args:
             game_biz (str): The name of the game.
             region (str): The region in which the game is registered.
+
+        Raises:
+            ValueError: If `cookie_token` is not found in the cookies.
         """
+        stoken = self.cookies.get("cookie_token")
+        if stoken is None:
+            raise ValueError("cookie_token not found in cookies.")
         url = HK4E_LOGIN_URL.get_url(self.region)
         json = {
             "game_biz": game_biz,
