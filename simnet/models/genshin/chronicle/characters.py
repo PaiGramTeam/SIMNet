af3d2baa64e488a9045ae7cc0115a50dd086cf0b
洛水居室 <luoshuijs@outlook.com>
2023-05-05 10:44:48 +0800

    :memo: Update documentation

--- simnet/models/genshin/chronicle/characters.py
+++ simnet/models/genshin/chronicle/characters.py
@@ -23,9 +23,9 @@ class PartialCharacter(BaseCharacter):
     Represents a Genshin Impact character without any equipment.
 
     Attributes:
-    - level (int): The character's level.
-    - friendship (int): The character's friendship level (also known as fetter).
-    - constellation (int): The number of constellations that are currently active for the character.
+        level (int): The character's level.
+        friendship (int): The character's friendship level (also known as fetter).
+        constellation (int): The number of constellations that are currently active for the character.
     """
 
     level: int
@@ -38,15 +38,15 @@ class CharacterWeapon(APIModel):
     Represents a character's equipped weapon.
 
     Attributes:
-    - id (int): The weapon's ID number.
-    - icon (str): The URL of the weapon's icon.
-    - name (str): The name of the weapon.
-    - rarity (int): The rarity (number of stars) of the weapon.
-    - description (str): The description of the weapon.
-    - level (int): The level of the weapon.
-    - type (str): The type of the weapon (e.g. Sword, Bow, etc.).
-    - ascension (int): The ascension level of the weapon.
-    - refinement (int): The refinement level of the weapon.
+        id (int): The weapon's ID number.
+        icon (str): The URL of the weapon's icon.
+        name (str): The name of the weapon.
+        rarity (int): The rarity (number of stars) of the weapon.
+        description (str): The description of the weapon.
+        level (int): The level of the weapon.
+        type (str): The type of the weapon (e.g. Sword, Bow, etc.).
+        ascension (int): The ascension level of the weapon.
+        refinement (int): The refinement level of the weapon.
     """
 
     id: int
@@ -65,9 +65,9 @@ class ArtifactSetEffect(APIModel):
     Represents the effect of an artifact set.
 
     Attributes:
-    - pieces (int): The number of artifact pieces required to activate the effect.
-    - effect (str): The description of the effect.
-    - enabled (bool): Whether the effect is currently enabled for the character's equipped artifacts. Defaults to False.
+        pieces (int): The number of artifact pieces required to activate the effect.
+        effect (str): The description of the effect.
+        enabled (bool): Whether the effect is currently enabled for the character's equipped artifacts. Defaults to False.
     """
 
     pieces: int = Field(alias="activation_number")
@@ -80,9 +80,9 @@ class ArtifactSet(APIModel):
     Represents an artifact set.
 
     Attributes:
-    - id (int): The artifact set's ID number.
-    - name (str): The name of the artifact set.
-    - effects (List[ArtifactSetEffect]): The effects of the artifact set.
+        id (int): The artifact set's ID number.
+        name (str): The name of the artifact set.
+        effects (List[ArtifactSetEffect]): The effects of the artifact set.
     """
 
     id: int
@@ -95,14 +95,14 @@ class Artifact(APIModel):
     Represents a character's equipped artifact.
 
     Attributes:
-    - id (int): The artifact's ID number.
-    - icon (str): The URL of the artifact's icon.
-    - name (str): The name of the artifact.
-    - pos_name (str): The name of the artifact's position (e.g. Flower of Life, Plume of Death, etc.).
-    - pos (int): The position of the artifact (1-5).
-    - rarity (int): The rarity (number of stars) of the artifact.
-    - level (int): The level of the artifact.
-    - set (ArtifactSet): The artifact set that the artifact belongs to.
+        id (int): The artifact's ID number.
+        icon (str): The URL of the artifact's icon.
+        name (str): The name of the artifact.
+        pos_name (str): The name of the artifact's position (e.g. Flower of Life, Plume of Death, etc.).
+        pos (int): The position of the artifact (1-5).
+        rarity (int): The rarity (number of stars) of the artifact.
+        level (int): The level of the artifact.
+        set (ArtifactSet): The artifact set that the artifact belongs to.
     """
 
     id: int
@@ -120,12 +120,12 @@ class Constellation(APIModel):
     Represents a character constellation.
 
     Attributes:
-    - id (int): The ID number of the constellation.
-    - icon (str): The URL of the constellation's icon.
-    - pos (int): The position of the constellation (1-6).
-    - name (str): The name of the constellation.
-    - effect (str): The description of the effect.
-    - activated (bool): Whether the constellation is currently activated. Defaults to False.
+        id (int): The ID number of the constellation.
+        icon (str): The URL of the constellation's icon.
+        pos (int): The position of the constellation (1-6).
+        name (str): The name of the constellation.
+        effect (str): The description of the effect.
+        activated (bool): Whether the constellation is currently activated. Defaults to False.
     """
 
     id: int
@@ -146,9 +146,9 @@ class Outfit(APIModel):
     Represents an outfit of a character.
 
     Attributes:
-    - id (int): The ID number of the outfit.
-    - icon (str): The URL of the outfit's icon.
-    - name (str): The name of the outfit.
+        id (int): The ID number of the outfit.
+        icon (str): The URL of the outfit's icon.
+        name (str): The name of the outfit.
     """
 
     id: int
@@ -161,10 +161,10 @@ class Character(PartialCharacter):
     Represents a Genshin Impact character with equipment.
 
     Attributes:
-    - weapon (CharacterWeapon): The character's equipped weapon.
-    - artifacts (List[Artifact]): The character's equipped artifacts.
-    - constellations (List[Constellation]): The character's constellations.
-    - outfits (List[Outfit]): The character's outfits.
+        weapon (CharacterWeapon): The character's equipped weapon.
+        artifacts (List[Artifact]): The character's equipped artifacts.
+        constellations (List[Constellation]): The character's constellations.
+        outfits (List[Outfit]): The character's outfits.
     """
 
     weapon: CharacterWeapon
@@ -178,10 +178,10 @@ class Character(PartialCharacter):
         Determines which artifact set effects are enabled for the character's equipped artifacts.
 
         Args:
-        - artifacts (List[Artifact]): The character's equipped artifacts.
+            artifacts (List[Artifact]): The character's equipped artifacts.
 
         Returns:
-        - List[Artifact]: The character's equipped artifacts with their corresponding artifact set effects enabled if
+            List[Artifact]: The character's equipped artifacts with their corresponding artifact set effects enabled if
           applicable.
         """
         sets: Dict[int, List[Artifact]] = {}
