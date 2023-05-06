7a97d6026e4984e1055d4a26327a3f9d1a7a2aa1
xtaodada <xtao@xtaolink.cn>
2023-05-06 21:27:17 +0800

    fix sr test

--- tests/test_starrail.py
+++ tests/test_starrail.py
@@ -5,12 +5,22 @@ import pytest
 if TYPE_CHECKING:
     from simnet.client.starrail import StarRailClient
 
+player_id = None
+
 
 @pytest.mark.asyncio
 class TestStarRailClient:
     @staticmethod
+    async def test_get_starrail_record(starrail_client: "StarRailClient"):
+        global player_id
+        record = await starrail_client.get_record_card()
+        assert record is not None
+        assert record.uid > 0
+        player_id = record.uid
+
+    @staticmethod
     async def test_get_starrail_user(starrail_client: "StarRailClient"):
-        user = await starrail_client.get_starrail_user()
+        user = await starrail_client.get_starrail_user(player_id=player_id)
         assert user is not None
         assert user.stats.chest_num > 0
         assert len(user.characters) > 0
@@ -19,12 +29,12 @@ class TestStarRailClient:
 
     @staticmethod
     async def test_get_starrail_notes(starrail_client: "StarRailClient"):
-        notes = await starrail_client.get_starrail_notes()
+        notes = await starrail_client.get_starrail_notes(player_id=player_id)
         assert notes is not None
 
     @staticmethod
     async def test_get_starrail_characters(starrail_client: "StarRailClient"):
-        characters = await starrail_client.get_starrail_characters()
+        characters = await starrail_client.get_starrail_characters(player_id=player_id)
         assert len(characters.avatar_list) > 0
         character = characters.avatar_list[-1]
         assert character.id > 0
