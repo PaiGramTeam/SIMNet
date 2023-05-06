0bbbc6c80d72815a885989c535a2525f7d58787e
洛水居室 <luoshuijs@outlook.com>
2023-05-06 20:48:21 +0800

    :sparkles: Add Get Record Cards

--- simnet/models/lab/record.py
+++ simnet/models/lab/record.py
@@ -235,9 +235,7 @@ class BaseRecordCard(Account):
         Returns:
             Dict[str, Any]: The data as a dictionary.
         """
-        return {
-            d.name: (int(d.value) if d.value.isdigit() else d.value) for d in self.data
-        }
+        return {d.name: (int(d.value) if d.value.isdigit() else d.value) for d in self.data}
 
 
 class GenshinRecordCard(BaseRecordCard):
@@ -403,20 +401,23 @@ class RecordCard(BaseRecordCard):
     """
 
     def __new__(cls, **kwargs: Any) -> BaseRecordCard:
-        """Creates the appropriate record card.
+        """Creates an appropriate record card instance based on the provided game ID.
 
         Args:
             **kwargs: The arguments passed in to create the record card.
 
         Returns:
-            BaseRecordCard: The record card object.
+            BaseRecordCard: An instance of a subclass of `BaseRecordCard` based on the provided game ID.
+
+        Raises:
+            ValueError: If the provided game ID is invalid.
         """
         game_id = kwargs.get("game_id", 0)
         if game_id == 1:
-            cls = HonkaiRecordCard  # skipcq:  PYL-W0642
+            return HonkaiRecordCard(**kwargs)
         elif game_id == 2:
-            cls = GenshinRecordCard  # skipcq:  PYL-W0642
+            return GenshinRecordCard(**kwargs)
         elif game_id == 6:
-            cls = StarRailRecodeCard  # skipcq:  PYL-W0642
+            return StarRailRecodeCard(**kwargs)
 
-        return super().__new__(cls)  # skipcq: PYL-E1120
+        raise ValueError(f"Invalid game ID provided: {game_id}")
