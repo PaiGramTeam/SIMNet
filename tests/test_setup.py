c38e708364242eec4a68b54618f7356f0d3aed56
洛水居室 <luoshuijs@outlook.com>
2023-05-09 16:13:10 +0800

    :white_check_mark: Add test build function and skip tests when not enabled
    To avoid unexpected impacts on the build system under certain conditions, we added a new test build function and used it for specific test scenarios. The function skips the test unless a flag named `test_build` is passed and set to `True`.

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,5 +1,9 @@
 import os
 
+import pytest
 
-def test_build():
+
+def test_build(test_build: bool):
+    if not test_build:
+        pytest.skip("TEST_BUILD not enabled")
     assert os.system("python setup.py") == 0  # pragma: no cover
