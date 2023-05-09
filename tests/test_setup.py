3b60c3f0104fe966430e96d356df6666d510464f
洛水居室 <luoshuijs@outlook.com>
2023-05-09 15:55:43 +0800

    :art: Update Code

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,9 +1,5 @@
 import os
 
-import pytest
 
-
-def test_build(test_build: bool):
-    if not test_build:
-        pytest.skip("TEST_BUILD not enabled")
+def test_build():
     assert os.system("python setup.py") == 0  # pragma: no cover
