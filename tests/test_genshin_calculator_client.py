39082b4bcd19c2273492682888746fba19f0ce02
洛水居室 <luoshuijs@outlook.com>
2023-05-12 11:53:07 +0800

    :white_check_mark: Update skip reason in client fixture

--- tests/test_genshin_calculator_client.py
+++ tests/test_genshin_calculator_client.py
@@ -14,7 +14,7 @@ if TYPE_CHECKING:
 @pytest_asyncio.fixture
 async def calculator_client(genshin_player_id: int, account_id: int, region: "Region", cookies: "Cookies"):
     if genshin_player_id is None:
-        pytest.skip("Test case test_starrail_battle_chronicle_client skipped: No starrail player id set.")
+        pytest.skip("Test case test_starrail_battle_chronicle_client skipped: No genshin player id set.")
     async with CalculatorClient(
         player_id=genshin_player_id,
         cookies=cookies,
