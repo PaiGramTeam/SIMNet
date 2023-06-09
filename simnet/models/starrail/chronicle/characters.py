45f425608927c29c9ccc7592bed672b678662e55
xtaodada <xtao@xtaolink.cn>
2023-06-09 22:44:00 +0800

    :sparkles: Add more starrail func

--- simnet/models/starrail/chronicle/characters.py
+++ simnet/models/starrail/chronicle/characters.py
@@ -1,32 +1,21 @@
-from typing import Optional, List
+"""Starrail chronicle character."""
+from typing import List, Optional
 
 from simnet.models.base import APIModel
-from simnet.models.starrail.character import BaseCharacter
+
+from .. import character
+
+__all__ = [
+    "StarRailEquipment",
+    "Rank",
+    "Relic",
+    "StarRailDetailCharacter",
+    "StarRailDetailCharacters",
+]
 
 
-class PartialCharacter(BaseCharacter):
-    """A character without any equipment.
-
-    Attributes:
-        level (int): The level of the character.
-        rank (int): The rank of the character.
-    """
-
-    level: int
-    rank: int
-
-
-class Equipment(APIModel):
-    """An equipment model used in StarRailDetailCharacter.
-
-    Attributes:
-        id (int): The ID of the equipment.
-        level (int): The level of the equipment.
-        rank (int): The rank of the equipment.
-        name (str): The name of the equipment.
-        desc (str): The description of the equipment.
-        icon (str): The icon of the equipment.
-    """
+class StarRailEquipment(APIModel):
+    """Character equipment."""
 
     id: int
     level: int
@@ -37,17 +26,7 @@ class Equipment(APIModel):
 
 
 class Relic(APIModel):
-    """A relic model used in StarRailDetailCharacter.
-
-    Attributes:
-        id (int): The ID of the relic.
-        level (int): The level of the relic.
-        pos (int): The position of the relic.
-        name (str): The name of the relic.
-        desc (str): The description of the relic.
-        icon (str): The icon of the relic.
-        rarity (int): The rarity of the relic.
-    """
+    """Character relic."""
 
     id: int
     level: int
@@ -59,16 +38,7 @@ class Relic(APIModel):
 
 
 class Rank(APIModel):
-    """A rank model used in StarRailDetailCharacter.
-
-    Attributes:
-        id (int): The ID of the rank.
-        pos (int): The position of the rank.
-        name (str): The name of the rank.
-        icon (str): The icon of the rank.
-        desc (str): The description of the rank.
-        is_unlocked (bool): Whether the rank is unlocked.
-    """
+    """Character rank."""
 
     id: int
     pos: int
@@ -78,29 +48,17 @@ class Rank(APIModel):
     is_unlocked: bool
 
 
-class StarRailDetailCharacter(PartialCharacter):
-    """A detailed character model used in StarShipDetailCharacters.
-
-    Attributes:
-        image (str): The image of the character.
-        equip (Optional[Equipment]): The equipment of the character, if any.
-        relics (List[Relic]): The relics of the character.
-        ornaments (List[Relic]): The ornaments of the character.
-        ranks (List[Rank]): The ranks of the character.
-    """
+class StarRailDetailCharacter(character.StarRailPartialCharacter):
+    """StarRail character with equipment and relics."""
 
     image: str
-    equip: Optional[Equipment]
+    equip: Optional[StarRailEquipment]
     relics: List[Relic]
     ornaments: List[Relic]
     ranks: List[Rank]
 
 
-class StarShipDetailCharacters(APIModel):
-    """A model containing a list of detailed characters used in the StarShipDetail API.
-
-    Attributes:
-        avatar_list (List[StarRailDetailCharacter]): The list of detailed characters.
-    """
+class StarRailDetailCharacters(APIModel):
+    """StarRail characters."""
 
     avatar_list: List[StarRailDetailCharacter]
