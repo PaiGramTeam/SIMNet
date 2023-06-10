888c729e1fea84935cec6ea8d5623d4a7347ef50
洛水居室 <luoshuijs@outlook.com>
2023-06-10 16:29:15 +0800

    :art: Exported `Region` and `Game` to `simnet.__init__.py`

--- simnet/__init__.py
+++ simnet/__init__.py
@@ -1,4 +1,5 @@
 from simnet.client.genshin import GenshinClient
 from simnet.client.starrail import StarRailClient
+from simnet.utils.enum_ import Game, Region
 
-__all__ = ("StarRailClient", "GenshinClient")
+__all__ = ("StarRailClient", "GenshinClient", "Game", "Region")
