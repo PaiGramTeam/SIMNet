27325f27910870c416d2aae00b4f599ea99d9eb7
洛水居室 <luoshuijs@outlook.com>
2023-06-12 10:10:30 +0800

    :art: Add `GenshinClient` inheriting from `CalculatorClient`

--- simnet/client/genshin.py
+++ simnet/client/genshin.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from simnet.client.components.auth import AuthClient
+from simnet.client.components.calculator.genshin import CalculatorClient
 from simnet.client.components.chronicle.genshin import GenshinBattleChronicleClient
 from simnet.client.components.daily import DailyRewardClient
 from simnet.client.components.diary.genshin import GenshinDiaryClient
@@ -13,6 +14,7 @@ __all__ = ("GenshinClient",)
 
 
 class GenshinClient(
+    CalculatorClient,
     GenshinBattleChronicleClient,
     GenshinWishClient,
     GenshinDiaryClient,
--- simnet/client/genshin.pyi
+++ simnet/client/genshin.pyi
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from simnet.client.components.auth import AuthClient
+from simnet.client.components.calculator.genshin import CalculatorClient
 from simnet.client.components.chronicle.genshin import GenshinBattleChronicleClient
 from simnet.client.components.daily import DailyRewardClient
 from simnet.client.components.diary.genshin import GenshinDiaryClient
@@ -12,6 +13,7 @@ from simnet.utils.types import CookieTypes, HeaderTypes, TimeoutTypes
 
 
 class GenshinClient(
+    CalculatorClient,
     GenshinBattleChronicleClient,
     GenshinWishClient,
     GenshinDiaryClient,
