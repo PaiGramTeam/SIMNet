d13e2e28c96917affa3d24247c9f3be6a6e5991a
洛水居室 <luoshuijs@outlook.com>
2023-09-12 08:24:32 +0800

    :bookmark: Bump Version to v0.1.11

--- simnet/version.py
+++ simnet/version.py
@@ -29,5 +29,5 @@ class Version(NamedTuple):
         return version
 
 
-__version_info__ = Version(major=0, minor=1, micro=10, release_level="beta", serial=0)
+__version_info__ = Version(major=0, minor=1, micro=11, release_level="beta", serial=0)
 __version__ = str(__version_info__)
