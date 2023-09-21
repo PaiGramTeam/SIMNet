72a60bca40e4e7af2845d18497dd22a8e8c67856
xtaodada <xtao@xtaolink.cn>
2023-09-21 16:56:51 +0800

    :construction_worker: support auto update ds

--- compiler/ds/download.py
+++ compiler/ds/download.py
@@ -1,7 +1,40 @@
-from httpx import get
+from time import sleep
 
-version = get("https://bbs-api.miyoushe.com/misc/wapi/getLatestPkgVer?channel=miyousheluodi").json()["data"]["version"]
-apk_url = f"https://download-bbs.miyoushe.com/app/mihoyobbs_{version}_miyousheluodi.apk"
+from httpx import get
+import subprocess
+import sys
+
+url = sys.argv[1] if len(sys.argv) > 1 else "https://download-bbs.miyoushe.com"
+
+
+def retry(func):
+    def wrapper(*args, **kwargs):
+        for _ in range(10):
+            try:
+                return func(*args, **kwargs)
+            except Exception:
+                print("retrying...")
+                sleep(1)
+                continue
+
+    return wrapper
+
+
+@retry
+def get_version():
+    return get(
+        "https://bbs-api.miyoushe.com/misc/wapi/getLatestPkgVer?channel=miyousheluodi",
+        verify=False,
+    ).json()["data"]["version"]
+
+
+version = get_version()
+apk_url = f"{url}/app/mihoyobbs_{version}_miyousheluodi.apk"
 print(f"download version {version} from {apk_url}")
-with open("mihoyobbs.apk", "wb") as f:
-    f.write(get(apk_url).content)
+
+command = f"wget -O mihoyobbs.apk {apk_url}"
+process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, error = process.communicate()
+
+print("命令输出:\n", output.decode())
+print("错误信息:\n", error.decode())
