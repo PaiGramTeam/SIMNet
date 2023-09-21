79471195b5f4d7dc9289e1e12b5a76824a3ee8b0
xtaodada <xtao@xtaolink.cn>
2023-09-21 17:04:41 +0800

    fix deepsource

--- compiler/ds/download.py
+++ compiler/ds/download.py
@@ -8,20 +8,24 @@ url = sys.argv[1] if len(sys.argv) > 1 else "https://download-bbs.miyoushe.com"
 
 
 def retry(func):
-    def wrapper(*args, **kwargs):
+    """retry 10 times"""
+    def wrapper(*args, **kwargs) -> str:
+        """retry 10 times"""
         for _ in range(10):
             try:
                 return func(*args, **kwargs)
-            except Exception:
+            except Exception:  # skipcq: PYL-W0703
                 print("retrying...")
                 sleep(1)
                 continue
+        return None
 
     return wrapper
 
 
 @retry
 def get_version():
+    """get version"""
     return get(
         "https://bbs-api.miyoushe.com/misc/wapi/getLatestPkgVer?channel=miyousheluodi",
         verify=False,
@@ -35,7 +39,7 @@ apk_url = f"{url}/app/mihoyobbs_{version}_miyousheluodi.apk"
 print(f"download version {version} from {apk_url}")
 
 command = f"wget -O mihoyobbs.apk {apk_url}"
-process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # skipcq: BAN-B602
 output, error = process.communicate()
 
 print("命令输出:\n", output.decode())
