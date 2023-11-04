eb856c5f312785d49eeb630b207b2c0cb3d64d1d
xtaodada <xtao@xtaolink.cn>
2023-11-04 16:40:20 +0800

    :bookmark: Bump Version to v0.1.14

--- simnet/version.py
+++ simnet/version.py
@@ -29,5 +29,5 @@ class Version(NamedTuple):
         return version
 
 
-__version_info__ = Version(major=0, minor=1, micro=13, release_level="beta", serial=0)
+__version_info__ = Version(major=0, minor=1, micro=14, release_level="beta", serial=0)
 __version__ = str(__version_info__)
