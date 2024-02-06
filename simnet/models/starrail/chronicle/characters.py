c6f1c0ee993b5ec6ae4bc2d98912e4208b54e9be
xtaodada <xtao@xtaolink.cn>
2024-02-06 13:28:47 +0800

    :sparkles: Support starrail new characters

--- simnet/models/starrail/chronicle/characters.py
+++ simnet/models/starrail/chronicle/characters.py
@@ -1,5 +1,7 @@
 """Starrail chronicle character."""
-from typing import List, Optional
+from typing import List, Optional, Any, Dict
+
+from pydantic import validator
 
 from simnet.models.base import APIModel
 
@@ -25,6 +27,14 @@ class StarRailEquipment(APIModel):
     icon: str
 
 
+class RelicProp(APIModel):
+    """Character relic property."""
+
+    property_type: int
+    times: int
+    value: str
+
+
 class Relic(APIModel):
     """Character relic."""
 
@@ -35,6 +45,8 @@ class Relic(APIModel):
     desc: str
     icon: str
     rarity: int
+    main_property: RelicProp
+    properties: List[RelicProp]
 
 
 class Rank(APIModel):
@@ -48,6 +60,42 @@ class Rank(APIModel):
     is_unlocked: bool
 
 
+class CharacterProp(APIModel):
+    """Character property."""
+
+    property_type: int
+    base: str
+    add: str
+    final: str
+
+
+class SkillStage(APIModel):
+    """Character skill stage."""
+
+    desc: str
+    name: str
+    level: int
+    remake: str
+    item_url: str
+    is_activated: bool
+    is_rank_work: bool
+
+
+class Skill(APIModel):
+    """Character skill."""
+
+    point_id: int
+    point_type: int
+    item_url: str
+    level: int
+    is_activated: bool
+    is_rank_work: bool
+    pre_point: int
+    anchor: str
+    remake: str
+    skill_stages: List[SkillStage]
+
+
 class StarRailDetailCharacter(character.StarRailPartialCharacter):
     """StarRail character with equipment and relics."""
 
@@ -56,9 +104,77 @@ class StarRailDetailCharacter(character.StarRailPartialCharacter):
     relics: List[Relic]
     ornaments: List[Relic]
     ranks: List[Rank]
+    properties: List[CharacterProp]
+    skills: List[Skill]
+    base_type: int
+    figure_path: str
+
+
+class EquipWiki(APIModel):
+    """Equipment wiki."""
+
+    id: int
+    url: str
+
+
+class PropertyInfo(APIModel):
+    """Property info."""
+
+    property_type: int
+    name: str
+    icon: str
+    property_name_relic: str
+    property_name_filter: str
+
+
+class RecommendProperty(APIModel):
+    """Recommend property."""
+
+    id: int
+    recommend_relic_properties: List[int]
+    custom_relic_properties: List[int]
+    is_custom_property_valid: bool
+
+
+class RelicProperty(APIModel):
+    """Relic property."""
+
+    property_type: int
+    modify_property_type: int
 
 
 class StarRailDetailCharacters(APIModel):
     """StarRail characters."""
 
     avatar_list: List[StarRailDetailCharacter]
+    equip_wiki: List[EquipWiki]
+    property_info: List[PropertyInfo]
+    recommend_property: List[RecommendProperty]
+    relic_properties: List[RelicProperty]
+
+    @staticmethod
+    def _parse(v: Dict[str, Any], key: str = None, value_key: str = None) -> List[Dict[str, Any]]:
+        """Parse dict to list."""
+        new_list = []
+        for k, v in v.items():
+            if isinstance(v, str):
+                v = {value_key: v}
+            if key:
+                v[key] = k
+            new_list.append(v)
+        return new_list
+
+    @validator("equip_wiki", pre=True)
+    def parse_equip_wiki(cls, v: Dict[str, str]) -> List[Dict[str, str]]:
+        """Parse equip wiki."""
+        return cls._parse(v, "id", "url")
+
+    @validator("property_info", pre=True)
+    def parse_property_info(cls, v: Dict[str, str]) -> List[Dict[str, str]]:
+        """Parse property info."""
+        return cls._parse(v)
+
+    @validator("recommend_property", pre=True)
+    def parse_recommend_property(cls, v: Dict[str, str]) -> List[Dict[str, str]]:
+        """Parse recommend property."""
+        return cls._parse(v, "id")
