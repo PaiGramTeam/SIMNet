c71fb05dbb3cc813361583d4c787ec770a5e8bab
xtaodada <xtao@xtaolink.cn>
2024-07-12 05:06:01 +0000

    :bento: Update ds salt

--- simnet/utils/ds.py
+++ simnet/utils/ds.py
@@ -9,9 +9,9 @@ from typing import Any, Optional
 from simnet.utils.enums import Region
 from simnet.utils.types import QueryParamTypes
 
-MIYOUSHE_VERSION = "2.72.2"
-MIYOUSHE_APP_DS = "KsQAfcXzHdMjoQQqjnPXVTXAygltZbSI"
-MIYOUSHE_WEB_DS = "tQURIpqhPqvFBNLHqoioILMifLtnyjIt"
+MIYOUSHE_VERSION = "2.73.1"
+MIYOUSHE_APP_DS = "WGtruoQrwczmsjLOPXzJLnaAYycsLavx"
+MIYOUSHE_WEB_DS = "YaROXkMnnbaQzjOmGPDmDKvpcavlZbli"
 
 
 class DSType(Enum):
