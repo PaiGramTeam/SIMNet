46c63a5cc64eba9f56a7926e3b1a24da120069fb
LuoShui <luoshuijs@outlook.com>
2024-12-03 21:45:18 +0800

    :wrench: Switch the package manager to uv

--- tests/test_setup.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import pytest
-
-
-def test_build(if_test_build: bool):
-    if not if_test_build:
-        pytest.skip("TEST_BUILD not enabled")
-    assert os.system("python setup.py") == 0  # pragma: no cover
