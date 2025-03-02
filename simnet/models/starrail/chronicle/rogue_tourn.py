2776edd32ece531cdc94b35df87d6355bb553e45
xtaodada <xtao@xtaolink.cn>
2025-03-02 23:55:04 +0800

    :bug: Update new starrail rogue tourn models

--- simnet/models/starrail/chronicle/rogue_tourn.py
+++ simnet/models/starrail/chronicle/rogue_tourn.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from simnet.models.base import APIModel
 from simnet.models.starrail.character import RogueCharacter
 from simnet.models.starrail.chronicle.base import PartialTime
@@ -13,7 +15,7 @@ class StarRailRogueTournCommonInfo(APIModel):
     """Star Rail Tourn Common Info"""
 
     area_id: int
-    difficulty: int
+    difficulty: Optional[int] = 0
     exist_data: bool
 
     @property
@@ -22,18 +24,53 @@ class StarRailRogueTournCommonInfo(APIModel):
         return self.area_id % 10
 
 
+class StarRailRogueTournCommonInfoV2(StarRailRogueTournCommonInfo):
+    """
+    Represents the version 2 of the common information for the Star Rail Rogue Tournament.
+
+    Attributes:
+    - level (int): The level of the tournament.
+    - id (int): The unique identifier for the tournament.
+    """
+
+    level: int
+    id: int
+
+
 class StarRailRogueTournRecordBrief(APIModel):
     """StarRail Rogue Tourn Record Brief"""
 
     title: str
     icon: str
-    color: str
-    common_info: StarRailRogueTournCommonInfo
+    color: Optional[str] = None
+    common_info: Optional[StarRailRogueTournCommonInfo] = None
+    common_info_v2: Optional[StarRailRogueTournCommonInfoV2] = None
 
     @property
     def exist_data(self) -> bool:
         """Check if the record exists."""
-        return self.common_info.exist_data
+        if self.common_info:
+            return self.common_info.exist_data
+        if self.common_info_v2:
+            return self.common_info_v2.exist_data
+        return False
+
+
+class StarRailRogueTournBasicSeason(APIModel):
+    """
+    Represents the basic season information for the Star Rail Rogue Tournament.
+
+    Attributes:
+    - season_id (int): The unique identifier for the season.
+    - season_level (int): The level of the season.
+    - season_name (str): The name of the season.
+    - icon (str): The icon representing the season.
+    """
+
+    season_id: int
+    season_level: int
+    season_name: str
+    icon: str
 
 
 class StarRailRogueTournBasic(APIModel):
@@ -49,6 +86,10 @@ class StarRailRogueTournBasic(APIModel):
     normal_record_brief: StarRailRogueTournRecordBrief
     weekly_record_brief: StarRailRogueTournRecordBrief
     finished_weekly: bool
+    rogue_tourn_exp_is_full: Optional[bool] = False
+    titan_total: Optional[int] = None
+    titan_current: Optional[int] = None
+    season_title_list: Optional[list[StarRailRogueTournBasicSeason]] = None
 
 
 class RogueTournMiracle(RogueMiracle):
@@ -67,6 +108,25 @@ class RogueTournFormula(APIModel):
     formula_category: int
 
 
+class RogueTournTitanBless(APIModel):
+    """
+    Represents a Titan Blessing in the Star Rail Rogue Tournament.
+
+    Attributes:
+    - id (int): The unique identifier for the Titan Blessing.
+    - name (str): The name of the Titan Blessing.
+    - icon (str): The icon representing the Titan Blessing.
+    - rarity (int): The rarity level of the Titan Blessing.
+    - type (int): The type category of the Titan Blessing.
+    """
+
+    id: int
+    name: str
+    icon: str
+    rarity: int
+    type: int
+
+
 class StarRailRogueTournRecord(APIModel):
     """Star Rail Rogue Tourn Record"""
 
@@ -77,7 +137,11 @@ class StarRailRogueTournRecord(APIModel):
     miracles: list[RogueTournMiracle]
     hex_miracles: list[RogueTournMiracle]
     formula_list: list[RogueTournFormula]
-    common_info: StarRailRogueTournCommonInfo
+    common_info: Optional[StarRailRogueTournCommonInfo] = None
+    easy_mode: Optional[int] = None
+    rank_mode: Optional[str] = None
+    titan_bless_list: Optional[list[RogueTournTitanBless]] = None
+    common_info_v2: Optional[StarRailRogueTournCommonInfoV2] = None
 
 
 class StarRailRogueTournDetail(APIModel):
