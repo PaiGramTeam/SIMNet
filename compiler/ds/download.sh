ccd64e57f7aebac428fb4afa439ae097e5d5db42
xtaodada <xtao@xtao.de>
2026-04-10 22:56:40 +0800

    :construction_worker: Fix action download apk

--- compiler/ds/download.sh
+++ compiler/ds/download.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# shellcheck disable=SC2154
-jsonData="$(curl -X POST 'https://yybadaccess.3g.qq.com/v2/dc_pcyyb_official' -H 'Content-Type: application/json' -d '{"head":{"cmd":"dc_pcyyb_official","authInfo":{"businessId":"AuthName"},"expSceneIds":"92215","hostAppInfo":{"scene":"game_detail"}},"body":{"bid":"yybhome","offset":0,"size":10,"preview":false,"listS":{"region":{"repStr":["CN"]},"pkgname":{"repStr":["com.mihoyo.hyperion"]}},"layout":"yybn_game_basic_info"}}')"
-url="$(echo "$jsonData" | jq --raw-output '.data.components[0].data.itemData[0].download_url')"
-curl -o mihoyobbs.apk "$url"
+for i in {1..3}
+do
+  wget -O mihoyobbs.apk "http://appstore.vivo.com.cn/appinfo/downloadApkFile-h5appstore?id=2808879"
+  if [ $? -eq 0 ]; then
+    break
+  fi
+done
+ls -l
